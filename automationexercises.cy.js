61c4b36256849b17b1e61b9bdf9b43ab00803d32
César Olivieri <cesar.olivieri82@gmail.com>
2023-02-24 18:06:13 -0300

    Algunos cambios en beforeEach y en todos los TCs (1 a 5)

--- automationexercises.cy.js
+++ automationexercises.cy.js
@@ -5,7 +5,9 @@ describe("Automation Exercises", ()=>{
     beforeEach(()=>{
         cy.visit("https://automationexercise.com/");
         cy.title().should("eq", "Automation Exercise");
+        cy.xpath("//*[contains(text(), 'Home')]").should("be.visible");
         cy.get("a[href='/login']").click();
+        cy.xpath("//*[contains(text(), 'New User Signup!')]").should("be.visible");
 
         Cypress.on("uncaught:exception", (err, runnable)=>{
             return false;
@@ -13,7 +15,7 @@ describe("Automation Exercises", ()=>{
     })
 
     it("TC1: Registrar usuario", ()=>{
-        registrarUsuario();
+        registrarUsuario("Alan Lorenzo", "alan_lorenzo@hotmail.com");
 
         cy.get("a[href='/delete_account']").click();
         cy.get("h2[data-qa='account-deleted']").should("be.visible");
@@ -21,8 +23,7 @@ describe("Automation Exercises", ()=>{
     })
 
     it('TC2: Logear usuario con email y password correctos', ()=>{
-        registrarUsuario();
-        //cy.xpath("//*[contains(text(), 'Login to your account')]").should("be.visible");
+        registrarUsuario("Alan Lorenzo", "alan_lorenzo@hotmail.com");
         //cy.get("input[data-qa='login-email']").type("alan@hotmail.com");
         //cy.get("input[data-qa='login-password']").type("12345");
         //cy.get("button[data-qa='login-button']").click();
@@ -31,8 +32,8 @@ describe("Automation Exercises", ()=>{
         cy.get("h2[data-qa='account-deleted']").should("be.visible");
     })
 
-    it.only('TC3: Logear usuario con email y password incorrectos', ()=>{
-        registrarUsuario();
+    it('TC3: Logear usuario con email y password incorrectos', ()=>{
+        registrarUsuario("Alan Lorenzo", "alan_lorenzo@hotmail.com");
         cy.xpath("//*[contains(text(), ' Logout')]").click();
         cy.get("a[href='/login']").click();
         cy.xpath("//*[contains(text(), 'Login to your account')]").should("be.visible");
@@ -42,16 +43,31 @@ describe("Automation Exercises", ()=>{
         cy.xpath("//*[contains(text(), 'Your email or password is incorrect!')]").should("be.visible");
     })
 
-    function registrarUsuario(){
-        let usuario = 'Alan Lorenzo';
+    it('TC4: Deslogear usuario', ()=>{
+        registrarUsuario("Alan Lorenzo", "alan_lorenzo@hotmail.com");
+        cy.xpath("//*[contains(text(), ' Logout')]").click();
+        cy.xpath("//*[contains(text(), 'Login to your account')]").should("be.visible");
+    })
 
-        cy.xpath("//*[contains(text(), 'New User Signup!')]").should("be.visible");
+    it.only('TC5: Registrar usuario con un email ya registrado', ()=>{
+        registrarUsuario("Alan Lorenzo", "alanl@hotmail.com");
+        cy.xpath("//*[contains(text(), ' Logout')]").click();
+        cy.xpath("//*[contains(text(), 'Login to your account')]").should("be.visible");
+        cy.get("a[href='/login']").click();
+        cy.get("input[data-qa='signup-name']").type("Alan Lienzo");
+        cy.get("input[data-qa='signup-email']").type("alanl@hotmail.com");
+        cy.get("button[data-qa='signup-button']").click();
+        cy.xpath("//*[contains(text(), 'Email Address already exist!')]").should("be.visible");
+    })
+
+    function registrarUsuario(usuario, email){
+       
         cy.get("input[data-qa='signup-name']").type(usuario);
-        cy.get("input[data-qa='signup-email']").type("alan@hotmail.com");
+        cy.get("input[data-qa='signup-email']").type(email);
         cy.get("button[data-qa='signup-button']").click();
         cy.xpath("//*[contains(text(), 'Enter Account Information')]").should("be.visible");
         cy.get("#id_gender1").click();
-        cy.get("#name").type(usuario);
+        //cy.get("#name").type(usuario);
         cy.get("#password").type("12345");
         cy.get("#days").select(9).should("be.visible");
         cy.get("#months").select("May").should("be.visible");
